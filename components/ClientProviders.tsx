0526e28567f681928c378bef92832eb58b507b9f
Seiano <44127890+Seiano@users.noreply.github.com>
2026-01-18 15:54:22 +0800

    Create ClientProviders.tsx

---
+++ components/ClientProviders.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { AIOperationProvider } from './AIOperationProvider'
+
+export function ClientProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <AIOperationProvider>
+      {children}
+    </AIOperationProvider>
+  )
+}
